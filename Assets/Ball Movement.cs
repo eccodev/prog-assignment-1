1acedb223c4cbfb4239a2543849b5f11b27e6828
echo <auricom@outlook.com>
2024-10-17 17:20:26 -0400

    edit ballmovement

--- Assets/Ball Movement.cs
+++ Assets/Ball Movement.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
 
 public class BallMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.W)
-            _direction = Vector2.up;
-        }   else if (Input.GetKey(KeyCode.S)
-            _direction = Vector2.down;
-        }   else {
-                _direction = Vector2.zero;
-        }
+        AddStartingForce();
+    }
 
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y = Random.value < 0.5f ? -1.0f : 1.0f; //temporary
+    }
     // Update is called once per frame
     void Update()
     {
