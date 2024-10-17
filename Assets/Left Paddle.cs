0b01a7436b8773103e078038bfbb90a375765de3
Robobanks12 <alex.macgregor@rogers.com>
2024-10-17 17:16:23 -0400

    Added Paddles and rough code for them

--- Assets/Left Paddle.cs
+++ Assets/Left Paddle.cs
@@ -7,9 +7,9 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (Input.GetKey(KeyCode.W)
+        if (Input.GetKey(KeyCode.AU)
             _direction = Vector2.up;
-        }   else if (Input.GetKey(KeyCode.S)
+        }   else if (Input.GetKey(KeyCode.AD)
             _direction = Vector2.down;
         }   else {
                 _direction = Vector2.zero;
--- Assets/Right Paddle.cs
+++ Assets/Right Paddle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMovement : MonoBehaviour
+public class RightPaddle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
