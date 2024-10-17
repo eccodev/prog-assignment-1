1f0e6115d331ced879ce0d8201211813711ad729
echo <auricom@outlook.com>
2024-10-17 17:15:06 -0400

    ball movement in progress

--- Assets/Ball Movement.cs
+++ Assets/Ball Movement.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
 
 public class BallMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        AddStartingForce();
     }
 
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y Random.value < 0.5f ? Random.Range(-1.0f, 0.5f) : 
+            Random.Range(-1.0f, 
+    }
     // Update is called once per frame
     void Update()
     {
